78ce91d0754e909c10e0e057bb02c3f93eeeca5b
FahadQamar262 <fadiqamar99@gmail.com>
2017-08-04 12:45:07 -0700

    Gradle changes

--- app/src/main/java/com/example/dell/driverapp/MapsActivity.java
+++ app/src/main/java/com/example/dell/driverapp/MapsActivity.java
@@ -1,8 +1,5 @@
 package com.example.dell.driverapp;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -42,24 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng uni = new LatLng(24.940544, 67.120736);
-        mMap.addMarker(new MarkerOptions()
-                .position(uni)
-                .title("Marker in KU")
-
-        );
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uni, 18));
-        // mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
